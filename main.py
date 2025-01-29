341fbe577f3e1b39e765a7ce7323260b05aa9646
ada263 <ada263@gmail.com>
2025-01-29 19:15:52 +0100

    simple code refactor

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from services.weather_service import get_weather
 from services.excel_service import save_to_excel
+from gui.interface import generate_interface
 import time
-
-
-def start():
+import threading
+def fetch_data():
     while True:
-
         weather = get_weather()
         save_to_excel(weather)
-        print("zapisane dane")
         time.sleep(30)
-def 
+def start():
+    threading.Thread(target=fetch_data).start()
+    generate_interface()
+
 start()
